e2b6337d40e28e5eb432a6789ee2c228bf61e2ed
geenathWeerasingha <73776728+geenathWeerasingha@users.noreply.github.com>
2024-09-10 11:35:16 +0530

    Second Commit

--- LiveAssessment/Models/Variation.cs
+++ LiveAssessment/Models/Variation.cs
@@ -2,5 +2,12 @@
 {
     public class Variation
     {
+        public int VariationId { get; set; }
+        public string? VariationName { get; set; }
+        public decimal? Price { get; set; }
+        public string? Sku { get; set; }
+        public int Stock { get; set; }
+        public Product Product { get; set; }
+
     }
 }
